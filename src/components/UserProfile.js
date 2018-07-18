89d61ecd8bc5dd88d73e501b67a8fe219eda79a1
Kumar Chandranshu <chandranshu040@gmail.com>
2018-07-18 09:41:27 +0000

    initial commit

---
+++ src/components/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile =() => (
+      <div className="UserProfile">
+        <div className="User">
+          <div className="name">Chandranshu</div>
+          <div className="image"><img src="https://avatars3.githubusercontent.com/u/19606820?s=400&u=fff40b76a2f6118b31fb5211d40d3ab22712b3d5&v=4" alt="profile" /></div>
+        </div>
+      </div>
+);
+
+export default UserProfile;
